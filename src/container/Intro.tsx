0c766f731a0d630699ca7677c0374a16d8203412
Ifechukwu Peter <ifecko2dumbi@gmail.com>
2024-10-06 23:29:21 +0100

    prisma and vercel blob installation

--- src/container/Intro.tsx
+++ src/container/Intro.tsx
@@ -1,49 +1,49 @@
-"use client";
-import React from "react";
-// import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
-// import {
-//   PlayArrowOutlined,
-//   PauseCircleFilledRounded,
-// } from "@mui/icons-material";
+// "use client";
+// import React from "react";
+// // import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+// // import {
+// //   PlayArrowOutlined,
+// //   PauseCircleFilledRounded,
+// // } from "@mui/icons-material";
 
-//import meal from '../assets/';
-import "../styles/Intro.scss";
+// //import meal from '../assets/';
+// import "../styles/Intro.scss";
 
-const Intro = () => {
-  const [playVideo, setPlayVideo] = React.useState(false);
-  const vidRef = React.useRef();
+// const Intro = () => {
+//   const [playVideo, setPlayVideo] = React.useState(false);
+//   const vidRef = React.useRef();
 
-  return (
-    <div className="app__video">
-      {/* <video
-        ref={vidRef}
-        src={meal}
-        type="video/mp4"
-        loop
-        controls={false}
-        
-      /> */}
-      <div className="app__video-overlay flex__center">
-        {/* <div
-          className="app__video-overlay_circle flex__center"
-          onClick={() => {
-            setPlayVideo(!playVideo);
-            if (playVideo) {
-              vidRef.current.pause();
-            } else {
-              vidRef.current.play();
-            }
-          }}
-        >
-          {playVideo ? (
-            <PauseCircleFilledRounded  />
-          ) : (
-            <PlayArrowOutlined  />
-          )}
-        </div> */}
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div className="app__video">
+//       {/* <video
+//         ref={vidRef}
+//         src={meal}
+//         type="video/mp4"
+//         loop
+//         controls={false}
 
-export default Intro;
+//       /> */}
+//       <div className="app__video-overlay flex__center">
+//         {/* <div
+//           className="app__video-overlay_circle flex__center"
+//           onClick={() => {
+//             setPlayVideo(!playVideo);
+//             if (playVideo) {
+//               vidRef.current.pause();
+//             } else {
+//               vidRef.current.play();
+//             }
+//           }}
+//         >
+//           {playVideo ? (
+//             <PauseCircleFilledRounded  />
+//           ) : (
+//             <PlayArrowOutlined  />
+//           )}
+//         </div> */}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Intro;
